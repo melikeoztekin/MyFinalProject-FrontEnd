436160fd23b1c726cc48bade780dbe2818198880
Melike Öztekin <melikeoztekin06@gmail.com>
2021-03-25 02:28:08 +0300

    24 Mart 21.Ders sonu eklendi login işlemleri

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -5,39 +5,42 @@ import { CategoryService } from 'src/app/services/category.service';
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
-  styleUrls: ['./category.component.css'],
+  styleUrls: ['./category.component.css']
 })
 export class CategoryComponent implements OnInit {
-  categories: Category[] = [];
-  currentCategory : Category;
-  constructor(private categoryService: CategoryService) {}
+
+  categories : Category[]=[];
+  currentCategory :Category;
+  constructor(private categoryService:CategoryService) { }
 
   ngOnInit(): void {
     this.getCategories();
   }
 
   getCategories() {
-    this.categoryService.getProducts().subscribe((response) => {
-      this.categories = response.data;
-    });
+    this.categoryService.getCategories().subscribe(response=>{
+      this.categories = response.data
+    })   
   }
   setCurrentCategory(category:Category){
-    this.currentCategory=category;
+    this.currentCategory = category;
   }
+
   getCurrentCategoryClass(category:Category){
-    if(category == this.currentCategory){
-      return "list-group-item active";
-    }
-    else{
-      return "list-group-item";
+    if(category ==this.currentCategory){
+      return "list-group-item active"
+    }else{
+      return "list-group-item"
     }
   }
+
   getAllCategoryClass(){
-    if(!this.currentCategory){
-      return "list-group-item active";
-    }
-    else{
-      return "list-group-item";
-    }
+       if(!this.currentCategory){
+        return "list-group-item active"
+       }
+       else{
+        return "list-group-item"
+       }
   }
+
 }
--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -8,11 +8,12 @@ import { Category } from '../models/category';
   providedIn: 'root'
 })
 export class CategoryService {
-  
+
   apiUrl = 'https://localhost:44388/api/categories/getall';
+
   constructor(private httpClient: HttpClient) { }
 
-  getProducts():Observable<ListResponseModel<Category>> {
+  getCategories():Observable<ListResponseModel<Category>> {
     return this.httpClient.get<ListResponseModel<Category>>(this.apiUrl);
   }
 }
